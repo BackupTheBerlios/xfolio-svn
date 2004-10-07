ce542d6860d4ca5101c0e012955fb2f439c0bb9d
glorieux <glorieux@a350c5fa-cee5-0310-aa28-cf33534a31ed>
2004-10-07 15:16:16 +0000

    Drop the bin to be more light.

--- bin/mirror.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo [fr] Votre serveur doit être démarré si vous voulez un miroir de quelque chose
-echo [en] Your server must be started if you want to mirror something
-echo.
-httrack\httrack localhost/ -s0 -C0 -%k -%B -%l "fr, en, ar, *" -Q -q -o0 -%c1 -O "mirror"
-echo.
-echo [fr] Retrouvez votre site dans le répertoire localhost 
-echo [en] Find your site in localhost folder 
